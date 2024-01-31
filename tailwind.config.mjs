262075f6da53a52326b74f12e4db890b59f73bf9
orlancst <orlancssst@gmail.com>
2024-01-31 13:57:26 -0500

    Sitio para reservas carnaval de barranquilla

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,15 +5,32 @@ export default {
 		extend: {
 			backgroundImage: {
 				'fondo-byhours': "url('/src/assets/img/fondo_city.webp')",
+				'fondo-carnaval': "url('/src/assets/img/fondo_carnaval.webp')",
 			},
 			fontFamily: {
 				'roboto-bold': '"Roboto Bold", sans-serif',
 				'titilliumweb-semiBold': '"TitilliumWeb SemiBold", sans-serif',
+				'montserrat-bold': '"Montserrat Bold", Arial, sans-serif',
 				
 			},
 			listStyleType: {
 				"marker-icon": 'url("/src/assets/img/marker.png")',
 			},
+			animation: {
+                'textToRight': 'textToRight .65s cubic-bezier(.215,.61,.355,1) 30ms forwards',
+            },
+			keyframes: {
+				textToRight: {
+					'0%': {
+						opacity: '0',
+                        transform: 'translate3d(-10px,0,0)',
+                    },
+                    '100%': {
+						opacity: '1',
+                        transform: 'translate3d(0,0,0)',
+                    },
+				}
+			}
 		},
 	},
 	plugins: [],
